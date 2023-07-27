fe94a54befee361137e628112dc32ad816e55380
PricillFranken <108398406+PricillFranken@users.noreply.github.com>
2023-07-27 19:41:32 +0300

    Update men.lua

--- men.lua
+++ men.lua
@@ -208,7 +208,7 @@ local args = {
 }
 game:GetService("ReplicatedStorage").Remotes.Teams.TeamChanger:InvokeServer(unpack(args))
       wait(0.4)
-game.Players.LocalPlayer.Character:MoveTo(Vector3.new(prevpos))
+game.Players.LocalPlayer.Character:MoveTo(Vector3.new(prevvec))
 wait(0.5)
 game:GetService'StarterGui':SetCore('SendNotification', {
   Title = 'Notification',

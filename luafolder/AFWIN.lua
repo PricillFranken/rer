45119b6a794d926434456dd8bf7974242f0375fc
PricillFranken <108398406+PricillFranken@users.noreply.github.com>
2023-07-29 01:20:30 +0300

    Update AFWIN.lua

--- luafolder/AFWIN.lua
+++ luafolder/AFWIN.lua
@@ -10,8 +10,10 @@ while wait() do
    me.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.CFrame
    wait(2)
    fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.ProximityPrompt, 1, true)
-   wait(5)
-   fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.ProximityPrompt, 1, true)
+   wait(10)
+   me.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.CFrame
+   wait(1)
+   fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.ProximityPrompt, 1, true)
    wait(7)
    me.CFrame = game:GetService("Workspace").Outside.Tunnel.ExitDoor.Door.Keypad.CFrame
    wait(5)

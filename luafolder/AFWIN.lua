f4e752cb39fff820ed9420df274b79a0505f0705
PricillFranken <108398406+PricillFranken@users.noreply.github.com>
2023-07-29 01:31:22 +0300

    Update AFWIN.lua

--- luafolder/AFWIN.lua
+++ luafolder/AFWIN.lua
@@ -4,18 +4,17 @@ if not game:FindFirstChild("DestroyAbleScripts") then
       setp.Name = "DestroyAbleScripts" 
       end
 -- script after this line: --
-local me = game.Players.LocalPlayer.Character.HumanoidRootPart
-
 while wait() do
-   me.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.CFrame
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.CFrame
    wait(2)
    fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.ProximityPrompt, 1, true)
    wait(10)
-   me.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.CFrame
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.CFrame
    wait(1)
    fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.ProximityPrompt, 1, true)
    wait(7)
-   me.CFrame = game:GetService("Workspace").Outside.Tunnel.ExitDoor.Door.Keypad.CFrame
+   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Outside.Tunnel.ExitDoor.Door.Keypad.CFrame
    wait(5)
    fireproximityprompt(game:GetService("Workspace").Outside.Tunnel.ExitDoor.Door.Keypad.Attachment.ProximityPrompt, 1, true)
+   wait(10)
 end

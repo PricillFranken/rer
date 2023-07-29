01a794a0a7f217de547db8783a13d07e93b43d71
PricillFranken <108398406+PricillFranken@users.noreply.github.com>
2023-07-29 13:39:00 +0300

    Update AFWIN.lua

--- luafolder/AFWIN.lua
+++ luafolder/AFWIN.lua
@@ -1,17 +1,9 @@
 function gsoo() 
-if Value==true then 
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.CFrame
-   wait(2)
-   fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.Call.ProximityPrompt, 1, true)
-   wait(10)
-   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.CFrame
-   wait(1)
-   fireproximityprompt(game:GetService("Workspace").Map2.E_End3.EDoor2.E.ActivationPart.ProximityPrompt, 1, true)
-   wait(7)
-   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Outside.Tunnel.ExitDoor.Door.Keypad.CFrame
-   wait(5)
+if Value == true then 
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9978, 81, 10194)
+   wait(3)
    fireproximityprompt(game:GetService("Workspace").Outside.Tunnel.ExitDoor.Door.Keypad.Attachment.ProximityPrompt, 1, true)
-   wait(25)
+   wait(60)
 gsoo()
 end
 end
